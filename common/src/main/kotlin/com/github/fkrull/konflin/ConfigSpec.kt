99303aebcf0cbec77f1dc2cd88e9debdad555d2a
Felix Krull <f_krull@gmx.de>
2018-02-22 20:42:02 +0100

    Refactor ConfigSpec to new, more rigid API.

--- common/src/main/kotlin/com/github/fkrull/konflin/ConfigSpec.kt
+++ common/src/main/kotlin/com/github/fkrull/konflin/ConfigSpec.kt
@@ -2,20 +2,18 @@ package com.github.fkrull.konflin
 
 import com.github.fkrull.konflin.impl.DelegatingSetting
 
-interface ConfigSpec
-
-@Suppress("unused")
-fun <T> ConfigSpec.setting(
+interface ConfigSpec {
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> setting(
+        type: kotlin.reflect.KClass<T>,
         name: String,
-        configDelegate: (configSource: ConfigurationSource) -> T?,
-        defaultValueDelegate: () -> T?
-): Setting<T> = DelegatingSetting(name, configDelegate, defaultValueDelegate)
-
-@Suppress("UNCHECKED_CAST", "unused")
-inline fun <reified T: Any> ConfigSpec.setting(name: String, defaultValue: T? = null): Setting<T> =
-    when (T::class) {
-        String::class -> setting(name, { it.getString(name) }, { defaultValue })
-        Int::class -> setting(name, { it.getInt(name) }, { defaultValue })
-        Boolean::class -> setting(name, { it.getBoolean(name) }, { defaultValue })
-        else -> throw UnsupportedConfigTypeException("cannot use '${T::class}' as config type")
+        defaultValue: T? = null
+    ): Setting<T> = when (type) {
+        String::class -> DelegatingSetting(name, { it.getString(name) }, { defaultValue })
+        Int::class -> DelegatingSetting(name, { it.getInt(name) }, { defaultValue })
+        Boolean::class -> DelegatingSetting(name, { it.getBoolean(name) }, { defaultValue })
+        else -> throw UnsupportedConfigTypeException("cannot use '$type' as config type")
     } as Setting<T>
+}
+
+inline fun <reified T: Any> ConfigSpec.setting(name: String, defaultValue: T? = null): Setting<T> = setting(T::class, name, defaultValue)
