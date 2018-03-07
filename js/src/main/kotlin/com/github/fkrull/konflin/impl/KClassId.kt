024420015a101de3da0c1d765ba9f720412b67d5
Felix Krull <f_krull@gmx.de>
2018-03-08 00:55:30 +0100

    Add KClassId and platform-dependent way to get an ID uniquely identifying a class.

---
+++ js/src/main/kotlin/com/github/fkrull/konflin/impl/KClassId.kt
@@ -0,0 +1,7 @@
+package com.github.fkrull.konflin.impl
+
+import kotlin.reflect.KClass
+
+internal actual data class KClassId actual constructor(val value: Any)
+
+internal actual val <T : Any> KClass<T>.id: KClassId get() = KClassId(js)
