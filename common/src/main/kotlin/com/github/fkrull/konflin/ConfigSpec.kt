05736e7deee0e8996179a35fc537852c0de5cf90
Felix Krull <f_krull@gmx.de>
2018-03-08 00:58:31 +0100

    Switch ConfigSpec to (internal and fixed) map of converters.

--- common/src/main/kotlin/com/github/fkrull/konflin/ConfigSpec.kt
+++ common/src/main/kotlin/com/github/fkrull/konflin/ConfigSpec.kt
@@ -1,24 +1,35 @@
 package com.github.fkrull.konflin
 
-import com.github.fkrull.konflin.impl.ConverterBasedSetting
 import com.github.fkrull.konflin.converter.ConfigType
 import com.github.fkrull.konflin.converter.Converter
 import com.github.fkrull.konflin.converter.impl.IdentityConverter
+import com.github.fkrull.konflin.impl.ConverterBasedSetting
+import com.github.fkrull.konflin.impl.KClassId
+import com.github.fkrull.konflin.impl.id
 import kotlin.reflect.KClass
 
+private infix fun <T : Any> T?.orElse(other: () -> T): T = this ?: other()
+
 interface ConfigSpec {
-    @Suppress("RemoveExplicitTypeArguments", "UNCHECKED_CAST")
-    fun <T : Any> setting(
-        type: KClass<T>,
-        name: String,
-        defaultValue: T? = null
-    ): Setting<T> = when (type) {
-        String::class -> ConverterBasedSetting<String>(name, defaultValue as String?, IdentityConverter(ConfigType.Types.String) as Converter<String, Any>)
-        Int::class -> ConverterBasedSetting<Int>(name, defaultValue as Int?, IdentityConverter(ConfigType.Types.Int) as Converter<Int, Any>)
-        Boolean::class -> ConverterBasedSetting<Boolean>(name, defaultValue as Boolean?, IdentityConverter(ConfigType.Types.Boolean) as Converter<Boolean, Any>)
-        else -> throw UnsupportedConfigTypeException("cannot use '$type' as config type")
-    } as Setting<T>
+    private companion object {
+        private val converters: Map<KClassId, Converter<*, *>> = mapOf(
+            String::class.id to IdentityConverter(ConfigType.Types.String),
+            Int::class.id to IdentityConverter(ConfigType.Types.Int),
+            Boolean::class.id to IdentityConverter(ConfigType.Types.Boolean)
+        )
+
+        @Suppress("UNCHECKED_CAST")
+        private fun <T : Any> getConverter(type: KClass<T>): Converter<T, Any>? = converters[type.id] as Converter<T, Any>?
+    }
+
+    fun <T : Any> setting(type: KClass<T>, name: String, defaultValue: T? = null): Setting<T> = getConverter(type)
+        .orElse { throw UnsupportedConfigTypeException("cannot use '$type' as config type") }
+        .let { createSetting(name, defaultValue, it) }
+
+    private fun <T : Any> createSetting(name: String, defaultValue: T?, converter: Converter<T, Any>) =
+        ConverterBasedSetting(name, defaultValue, converter)
 }
 
-inline fun <reified T: Any> ConfigSpec.setting(name: String, defaultValue: T? = null): Setting<T>
-    = setting(T::class, name, defaultValue)
+inline fun <reified T : Any> ConfigSpec.setting(name: String, defaultValue: T? = null): Setting<T> =
+    setting(T::class, name, defaultValue)
+
